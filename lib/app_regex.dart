39dd25b21933db8800af67030404875dc2bf994f
Birame Sene <birametgod95@gmail.com>
2024-05-13 20:15:26 +0200

    add get x architecture

--- lib/app_regex.dart
+++
@@ -1,8 +0,0 @@
-class AppRegex {
-  static Authentication authentication = Authentication();
-}
-
-class Authentication {
-  String email = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-  String password = r'(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)';
-}
